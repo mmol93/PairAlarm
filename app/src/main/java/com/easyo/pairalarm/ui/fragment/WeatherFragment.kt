d05f5d4ed2cdf377fe7a8c6a9d2fd68c502a0248
mmol93 <ljws93@naver.com>
2022-05-17 15:35:01 +0900

    Built WeatherFragment UI(copy from Maid)

--- app/src/main/java/com/easyo/pairalarm/ui/fragment/WeatherFragment.kt
+++ app/src/main/java/com/easyo/pairalarm/ui/fragment/WeatherFragment.kt
@@ -6,13 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.easyo.pairalarm.R
+import com.easyo.pairalarm.databinding.FragmentWeatherBinding
 
 class WeatherFragment : Fragment() {
+    private lateinit var binding: FragmentWeatherBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentWeatherBinding.bind(view)
     }
 }
