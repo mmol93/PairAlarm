f4e8e2b3306b4bdea5ef2c437d9d1ebb8f3f6e8f
mmol93 <ljws93@naver.com>
2023-01-27 06:44:34 +0900

    SettingFragment 초기 셋팅 완료

--- app/src/main/java/com/easyo/pairalarm/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/easyo/pairalarm/ui/fragment/SettingFragment.kt
@@ -1,18 +1,17 @@
 package com.easyo.pairalarm.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.easyo.pairalarm.R
+import com.easyo.pairalarm.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+class SettingFragment : Fragment(R.layout.fragment_setting) {
+    private lateinit var binding: FragmentSettingBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        DataBindingUtil.bind<FragmentSettingBinding>(view)?.let { binding = it } ?: return
     }
 }
