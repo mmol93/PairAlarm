6a2e6cb45099d632006e1d255888f3a093c07a97
mmol93 <ljws93@naver.com>
2023-03-14 10:24:47 +0900

    브로드캐스트 리시버 테스트 업데이트

--- app/src/androidTest/java/com/easyo/pairalarm/broadcastTest/AlarmBroadCastTest.kt
+++ app/src/androidTest/java/com/easyo/pairalarm/broadcastTest/AlarmBroadCastTest.kt
@@ -5,27 +5,30 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.easyo.pairalarm.broadcast.AlarmReceiver
 import junit.framework.TestCase.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AlarmBroadCastTest {
+    private val context = InstrumentationRegistry.getInstrumentation().context
+    private lateinit var receiver: AlarmReceiver
+
+    @Before
+    fun setup() {
+        receiver = AlarmReceiver()
+    }
 
     @Test
     fun test_MY_PACKAGE_REPLACED_OnReceive() {
-        val context = InstrumentationRegistry.getInstrumentation().context
         val intent = Intent("android.intent.action.MY_PACKAGE_REPLACED")
-        val receiver = AlarmReceiver()
         receiver.onReceive(context, intent)
-
         assertEquals("android.intent.action.MY_PACKAGE_REPLACED", intent.action)
     }
 
     @Test
     fun test_BOOT_COMPLETED_OnReceive() {
-        val context = InstrumentationRegistry.getInstrumentation().context
         val intent = Intent("android.intent.action.BOOT_COMPLETED")
-        val receiver = AlarmReceiver()
         receiver.onReceive(context, intent)
 
         assertEquals("android.intent.action.BOOT_COMPLETED", intent.action)
