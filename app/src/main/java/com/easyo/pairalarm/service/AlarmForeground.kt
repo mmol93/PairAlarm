8727136eac0c8e6acf57e9c10f0aca34b9cf1788
mmol93 <ljws93@naver.com>
2023-03-27 10:32:02 +0900

    (테스트) 포그라운드에서 PeriodicWorkRequest를 사용하여 정기적으로 브로드캐스트를 설정하게 하기

--- app/src/main/java/com/easyo/pairalarm/service/AlarmForeground.kt
+++ app/src/main/java/com/easyo/pairalarm/service/AlarmForeground.kt
@@ -3,11 +3,15 @@ package com.easyo.pairalarm.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import com.easyo.pairalarm.util.ALARM_NOTI_ID
 import com.easyo.pairalarm.util.NEXT_ALARM_NOTIFICATION_TEXT
 import com.easyo.pairalarm.util.getNotificationBuilder
-import com.easyo.pairalarm.util.resetAllAlarms
+import com.easyo.pairalarm.worker.NextAlarmWorker
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * 포그라운드 서비스가 브로드캐스팅이 강제로 취소되는 것을 막아줌
@@ -23,7 +27,13 @@ class AlarmForeground : Service() {
                 intent?.getStringExtra(NEXT_ALARM_NOTIFICATION_TEXT) ?: ""
             ).build()
         )
-        resetAllAlarms(applicationContext)
+
+//        resetAllAlarms(applicationContext)
+
+        val alarmTimeWorkRequest: WorkRequest =
+            PeriodicWorkRequest.Builder(NextAlarmWorker::class.java, 4, TimeUnit.HOURS).build()
+
+        WorkManager.getInstance(applicationContext).enqueue(alarmTimeWorkRequest)
 
         return START_REDELIVER_INTENT
     }
